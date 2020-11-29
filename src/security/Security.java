13e72c0f8e4fb720efe7b59220014a55c5deb661
Aleksander-git-hub <aleksander.chugunoff@yandex.ru>
2020-11-30 01:53:40 +0300

    MD5

---
+++ src/security/Security.java
@@ -0,0 +1,24 @@
+package security;
+
+/*
+SHA-1
+MD5
+ */
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Security
+{
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        String str = "Hello world!";
+        MessageDigest digest = MessageDigest.getInstance("SHA-1");
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        byte[] bytes = md5.digest(str.getBytes());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (byte b : bytes) {
+            stringBuilder.append(String.format("%02X ", b));
+        }
+        System.out.println(stringBuilder.toString());
+    }
+}
